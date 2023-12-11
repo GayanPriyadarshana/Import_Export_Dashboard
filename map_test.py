e7ebebb20005cc491046f44560600cc27b32e2ed
gayan <hewagegayanpriyadarshana@gmail.com>
2023-12-11 21:34:30 +0530

    changing coutries dynamically

--- map_test.py
+++ map_test.py
@@ -12,63 +12,66 @@ def interpolate_points(lon1, lat1, lon2, lat2, num_points):
 file_path = 'Group2_Sample.xlsx'
 data = pd.read_excel(file_path)
 
-# Select the year and country (for example, 2020 and Sri Lanka)
+print(data)
+# Select the year for the analysis
 selected_year = 2020
-selected_country = 'Sri Lanka'
-filtered_data = data[(data['Year'] == selected_year) & (data['Partner Country (Importer)'] == selected_country)]
+data = data[data['Year'] == selected_year]
 
-# Assuming the US coordinates as the starting point
-us_lat, us_lon = 37.0902, -95.7129
 
-# Taking the mean of the coordinates for Sri Lanka from the filtered data
-sl_lat = filtered_data['Importer_lat'].mean()
-sl_lon = filtered_data['Importer_lon'].mean()
-
+# Initialize figure
 fig = go.Figure()
 
-# Interpolate points for a straighter line
-num_intermediate_points = 100  # Increase the number for a straighter line
-interpolated_lons, interpolated_lats = interpolate_points(us_lon, us_lat, sl_lon, sl_lat, num_intermediate_points)
+# Get unique list of exporters and importers
+exporters = data['Reporting Country (Exporter)'].unique()
+importers = data['Partner Country (Importer)'].unique()
 
-# Add straighter line between the US and Sri Lanka
-fig.add_trace(go.Scattergeo(
-    lat = interpolated_lats,
-    lon = interpolated_lons,
-    mode = 'lines',
-    line = dict(width = 2, color = 'blue'),
-))
+# Iterate over each exporter-importer pair
+for exporter in exporters:
+    for importer in importers:
+        # Filter data for the current exporter-importer pair
+        exporter_data = data[data['Reporting Country (Exporter)'] == exporter]
+        importer_data = data[data['Partner Country (Importer)'] == importer]
+        
+        # Calculate mean coordinates for exporter and importer
+        exporter_mean = exporter_data[['Exporter_lon', 'Exporter_lat']].mean()
+        importer_mean = importer_data[['Importer_lon', 'Importer_lat']].mean()
+        
+        # Add line between exporter and importer
+        fig.add_trace(go.Scattergeo(
+            lon = [exporter_mean['Exporter_lon'], importer_mean['Importer_lon']],
+            lat = [exporter_mean['Exporter_lat'], importer_mean['Importer_lat']],
+            mode = 'lines',
+            line = dict(width = 2, color = 'blue'),
+        ))
 
-# Adding the arrow to the end of the line
-l = 1.1  # arrow length, adjust as needed
-widh = 0.010  # 2*widh is the width of the arrow base as a triangle
-
-# Calculate vector from US to Sri Lanka
-A = np.array([us_lon, us_lat])
-B = np.array([sl_lon, sl_lat])
-v = B - A
-w = v / np.linalg.norm(v)
-u = np.array([-v[1], v[0]])  # u orthogonal to w
-
-# Calculate points for the arrowhead
-P = B - l * w
-S = P - widh * u
-T = P + widh * u
-
-# Add the arrowhead as a filled triangle
-fig.add_trace(go.Scattergeo(
-    lon = [S[0], T[0], B[0], S[0]], 
-    lat = [S[1], T[1], B[1], S[1]], 
-    mode = 'lines',
-    fill = 'toself',
-    fillcolor = 'blue',
-    line_color = 'blue'
-))
+        
+        # Calculate the arrowhead for the line
+        l = 1.1  # arrow length, adjust as needed
+        widh = 0.010  # 2*widh is the width of the arrow base as a triangle
+        A = np.array(exporter_mean)
+        B = np.array(importer_mean)
+        v = B - A
+        w = v / np.linalg.norm(v)
+        u = np.array([-v[1], v[0]])  # u orthogonal to w
+        
+        # Calculate points for the arrowhead
+        P = B - l * w
+        S = P - widh * u
+        T = P + widh * u
+        
+        # Add the arrowhead as a filled triangle
+        fig.add_trace(go.Scattergeo(
+            lon = [S[0], T[0], B[0], S[0]],
+            lat = [S[1], T[1], B[1], S[1]],
+            mode = 'lines',
+            fill = 'toself',
+            fillcolor = 'blue',
+            line_color = 'blue'
+        ))
 
 # Update the layout to adjust the appearance
 fig.update_layout(
-    title = f'Trade Flow from US to {selected_country} in {selected_year}',
-    width = 900,
-    height = 750,
+    title = f'Trade Flows in {selected_year}',
     showlegend = False,
     geo = dict(
         projection_type = 'equirectangular',
