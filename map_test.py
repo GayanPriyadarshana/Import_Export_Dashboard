d74bd5635f1e1713cfec95e79f79777efa7c63ae
gayan <hewagegayanpriyadarshana@gmail.com>
2023-12-11 22:21:33 +0530

    line is straight and

--- map_test.py
+++ map_test.py
@@ -21,6 +21,9 @@ data = data[data['Year'] == selected_year]
 # Initialize figure
 fig = go.Figure()
 
+# Define mapbox style and token
+mapbox_style = "open-street-map" 
+
 # Get unique list of exporters and importers
 exporters = data['Reporting Country (Exporter)'].unique()
 importers = data['Partner Country (Importer)'].unique()
@@ -36,50 +39,58 @@ for exporter in exporters:
         exporter_mean = exporter_data[['Exporter_lon', 'Exporter_lat']].mean()
         importer_mean = importer_data[['Importer_lon', 'Importer_lat']].mean()
         
-        # Add line between exporter and importer
-        fig.add_trace(go.Scattergeo(
+        # Add straight line between exporter and importer using Scattermapbox
+        fig.add_trace(go.Scattermapbox(
             lon = [exporter_mean['Exporter_lon'], importer_mean['Importer_lon']],
             lat = [exporter_mean['Exporter_lat'], importer_mean['Importer_lat']],
             mode = 'lines',
             line = dict(width = 2, color = 'blue'),
         ))
 
-        
-        # Calculate the arrowhead for the line
-        l = 1.1  # arrow length, adjust as needed
-        widh = 0.010  # 2*widh is the width of the arrow base as a triangle
-        A = np.array(exporter_mean)
-        B = np.array(importer_mean)
-        v = B - A
-        w = v / np.linalg.norm(v)
-        u = np.array([-v[1], v[0]])  # u orthogonal to w
-        
-        # Calculate points for the arrowhead
-        P = B - l * w
-        S = P - widh * u
-        T = P + widh * u
-        
-        # Add the arrowhead as a filled triangle
-        fig.add_trace(go.Scattergeo(
-            lon = [S[0], T[0], B[0], S[0]],
-            lat = [S[1], T[1], B[1], S[1]],
-            mode = 'lines',
-            fill = 'toself',
-            fillcolor = 'blue',
-            line_color = 'blue'
+         # Add direction marker at the end of the line
+        fig.add_trace(go.Scattermapbox(
+            lon = [importer_mean['Importer_lon']],
+            lat = [importer_mean['Importer_lat']],
+            mode = 'markers',
+            marker = dict(size = 6, color = 'red')
         ))
 
-# Update the layout to adjust the appearance
+        
+        # # Calculate the arrowhead for the line
+        # l = 1.1  # arrow length, adjust as needed
+        # widh = 0.010  # 2*widh is the width of the arrow base as a triangle
+        # A = np.array(exporter_mean)
+        # B = np.array(importer_mean)
+        # v = B - A
+        # w = v / np.linalg.norm(v)
+        # u = np.array([-v[1], v[0]])  # u orthogonal to w
+        
+        # # Calculate points for the arrowhead
+        # P = B - l * w
+        # S = P - widh * u
+        # T = P + widh * u
+        
+        # # Add the arrowhead as a filled triangle
+        # fig.add_trace(go.Scattergeo(
+        #     lon = [S[0], T[0], B[0], S[0]],
+        #     lat = [S[1], T[1], B[1], S[1]],
+        #     mode = 'lines',
+        #     fill = 'toself',
+        #     fillcolor = 'blue',
+        #     line_color = 'blue'
+        # ))
+
+# Update the layout to adjust the appearance and set the mapbox style
 fig.update_layout(
     title = f'Trade Flows in {selected_year}',
     showlegend = False,
-    geo = dict(
-        projection_type = 'equirectangular',
-        showland = True,
-        landcolor = 'rgb(217, 217, 217)',
-        countrycolor = 'rgb(200, 200, 200)',
-        showcountries = True,
-        countrywidth = 1
+    mapbox = dict(
+        style = mapbox_style,
+        center = go.layout.mapbox.Center(
+            lat = 30,  # Center latitude for the initial map view
+            lon = 0    # Center longitude for the initial map view
+        ),
+        zoom = 1.2  # Initial zoom level
     )
 )
 
